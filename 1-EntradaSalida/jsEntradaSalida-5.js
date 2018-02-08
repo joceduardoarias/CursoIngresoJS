55e8da00c53abb030e0ad7f4f3b50fd2d0f83b8f
alumno <alumno>
2018-02-08 12:31:45 -0300

    Declarar variables, asignar valores a variables. Entrada y salidad de datos. prompt, alert. document.getElementById("").value

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,13 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+    var nombre;
+    var edad;
+    nombre = prompt("");
+    document.getElementById("elNombre").value = nombre;
+    edad = prompt("");
+    document.getElementById("laEdad").value = edad;
+    
+
 }
 
