486e745f682367aeb591f1a07f325c334e9e89a2
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-09 12:26:09 -0300

    Estructura de control if ejercicio 1, 2 , 3, 4.
    el ejercicio 4 no se ejecuta consultar intervalos con if y if else.

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+    var edad;
+    edad = document.getElementById("edad").value;
+    edad = parseInt(edad);
+
+    if(edad >= 18){
+        alert("usted es mayor de edad");
+    } alert("usted es menor de edad");
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+    if(edad )
 
 
 }//FIN DE LA FUNCIÓN
