aef6147ee1f25831f460eb7cbdb3d9f1ed561bd3
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-11 20:14:22 -0300

    Parciales resueltos 1, 2, 3.,
    Uso de prompt, alert, document.getElemetById. revisar soluciones matemáticas.

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+	var importe;
+	var aumento;
+	var resultado;
+
+	importe = prompt("Ingrese imprte:","");
+
+	importe = parseInt(importe);
+
+	aumento = importe * 0.21;
+	resultado = importe + aumento;
+
+	alert("El imp0rte final es: " + resultado);
 	
 }
 
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var largo;
+	var ancho;
+	var perimetro;
+	var cantidad;
+
+	largo = document.getElementById("ancho").value;
+	ancho = document.getElementById("largo").value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = 2 * (largo + ancho);
+	cantidad = perimetro * 6;
+
+	alert("la cantidad de hilo : " + cantidad);
 	
 }
 
