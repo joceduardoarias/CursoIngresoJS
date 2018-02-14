9f46b6117e3052d9659e53e1e0ab689cf26c8c27
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-13 21:31:09 -0300

    Resuelto
    Falta analizar mas el método math.random(); y su funcionamiento en le generación de números pseudoaleatorios. El ejercicio 10 es la integración con el if pero primero debemos aclarar las dudas acerca del método math.

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+
+	var numeroAleatorio;
+
+
+	// numeroAleatorio = Math.random(); // Con este metodo buscamos numero aleatorios entre 0 y 1, pero sin incluir el 1.
+
+	numeroAleatorio = ( Math.random() * 9 ) + 1;
+
+ 	numeroAleatorio = Math.round(numeroAleatorio);
+
+	alert("Numero aleatorio: " + numeroAleatorio );
 	
+	console.log(numeroAleatorio);
 
 }//FIN DE LA FUNCIÓN
