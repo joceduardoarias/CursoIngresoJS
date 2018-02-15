770a532312eed4ddee5b8fc72f9c8bd4ed13595c
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-15 12:22:01 -0300

    Math.random ( números aleatorios )
    Explicación y solución, ademas investigar sus usos.
     el código se ejecuta muy bien,

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+
+	numero = parseInt ( ( ( Math.random () * (11 - 1) ) + 1) );
+
+	if( numero >= 9 ){
+
+		
+		console.log("EXCELENTE"+ numero);
+
+	}else if( numero >= 4){
+
+		console.log("APROBO" + numero);
+	}else{
+
+		console.log("Vamos, la proxima se puede" + numero);
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,18 +1,10 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var  numero;
 
-	var numeroAleatorio;
+	numero = parseInt( ( (Math.random() * (11 - 1)) + 1 ) ); 
 
-
-	// numeroAleatorio = Math.random(); // Con este metodo buscamos numero aleatorios entre 0 y 1, pero sin incluir el 1.
-
-	numeroAleatorio = ( Math.random() * 9 ) + 1;
-
- 	numeroAleatorio = Math.round(numeroAleatorio);
-
-	alert("Numero aleatorio: " + numeroAleatorio );
-	
-	console.log(numeroAleatorio);
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
