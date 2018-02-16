87fd4189275fb472a98caae392f7635973835030
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-16 12:24:48 -0300

    instrucción switch
    todos los codigos están listos!!!

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,31 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    //tomo la edad  
+    var mesDelAño; 
+    
+    mesDelAño= document.getElementById('mes').value;
+
+    switch(mesDelAño)
+    {
+        case "Enero":
+            
+            alert("que comiences bien el año");
+            break;
+        
+        case "Marzo":
+            
+            alert("a clases");
+            break;
+        
+        case "Julio":
+        
+            alert("se vienen la vacaiones");
+            break;
+        case "Diciembre":
+        
+            alert("Felices fiestas");
+            break;
+    }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,33 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+        switch(mesDelAño)
+
+        {
+            case "Febrero":
+
+                alert("Este mes tiene 28 dias");
+                break;
+
+            case "Abril":
+            case "Junio":
+            case "Septiembre":
+            case "Noviembre":
+            
+                alert("Tiene 30 dias");
+                break;
+            
+            default:
+            
+                alert("Tiene 31 dias");
+                break;
+
+                
+        }
+            
 	
 
 
