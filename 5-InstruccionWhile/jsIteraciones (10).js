fe907da68d3ba595b2a2c259841b4634df9a9c40
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-23 12:09:40 -0300

    sentencia while 9, 10.
    El problema 9 se ejecuta bien, el 10 falta aclarar.

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,61 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador = 0;
+	var numero;
+	var negativos = 0;
+	var contadorNegativos = 0;
+	var positivos= 0;
+	var contadorPositivos = 0;
+	var contadorCeros = 0;
+	var contadorPares=0;
+	var promedioNegativos;
+	var promedioPositivos;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
-
-	while(respuesta!="no")
-	{
-		
 	
+	while(respuesta!="no")
+	{	
+		contador ++;
+		numero = prompt("Ingrese numero");
+		numero = parseInt(numero);
+		
+		if( numero%2 == 0 && numero != 0)
+		{
+			contadorPares++;
+		}
+		if( numero < 0 )
+		{
+			negativos = negativos + numero;
+			 contadorNegativos ++;
+		}
+		else
+		{
+			if( numero > 0)
+			{
+				positivos = positivos + numero;
+				contadorPositivos++;
+			}
+			else
+			{
+				contadorCeros ++;
+			}
+		}
+	
+		respuesta = prompt(" para continuar ingrese si / no");
 	}
+		promedioNegativos = negativos / contadorNegativos;
+		promedioPositivos = positivos / contadorPositivos;
+		
+		diferencia = positivos - negativos;
 
-
-
+		document.write("suma negativos :" + "" +negativos);
+		document.write("promedioNegativos:"+""+promedioNegativos);
+		document.write("promedioPositivos:"+""+promedioPositivos);
+		document.write("cantidad de ceros :"+""+ contadorCeros);
+		document.write("contadorPares:"+""+contadorPares);
+		document.write("diferencia :"+""+ diferencia);
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,42 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var maximo;
+	var minimo;
 	// declarar variables
 	
 	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
-	
+	while( respuesta != "no")
+	{	
+		contador++;
+		numero = prompt("ingrese numero");
+		numero = parseInt(numero);
+
+		if( contador == 1)
+		{
+			maximo = numero;
+			minimo = numero;
+		}
+		else
+		{
+
+		if(numero > maximo)
+		{
+			maximo = numero;					// sacar minimo o maxmo son dos cosas diferetntes por eso utilizamos dos if distintos.
+		}
+		if(numero < minimo)
+		{
+			minimo = numero;
+		}
+
+		}
+		//contador ++;								puedo ubicar el contador despues de las comparaciones y cambiar el valor del contador == 1 por contador == 0. 
+		respuesta = prompt("para salir ingrese; no");
 	}
 
-
-
+		document.getElementById("maximo").value = maximo;
+		document.getElementById("minimo").value = minimo;
 
 }//FIN DE LA FUNCIÓN
