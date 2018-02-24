3836e3dfe3d8c2ffcf119d334fd33fc86387aeb4
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-02-24 16:38:02 -0300

    validando con while
    un while dentro de otro while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -5,13 +5,24 @@ function Mostrar()
 	var acumulador=0;
 	var numero;
 
-	while(contador < 5)
-	{
-		contador = contador + 1;
-		numero = prompt("Ingrese un numero");
-		numero = parseInt(numero);
+	while(contador <5)
+	{	
 
-		acumulador = acumulador + numero;
+		contador = contador +1;
+		numero = prompt("Ingrese numero");
+		numero = parseInt(numero);
+		
+		while( numero < 1 || numero > 10 )
+		
+		{
+			numero = prompt("Ingrese numero"); // validando por lo general con while aqui en este caso le estoy diciendo al usuario 
+											   // que si el numero que ingresa no esta en 1 y 10, el programa le va pedir que  rectifique e ingrese
+											   // un numero entre 0 y 10. NOTA : EL contador no cuenta si el numero no esta dentro de 0 y 10. 
+			numero = parseInt(numero);
+		}	
+
+		acumulador = acumulador +numero;
+
 	}	
 
 	document.getElementById('suma').value=acumulador;
