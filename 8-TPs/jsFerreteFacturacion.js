3272eaea8587ad3eb6a763bd815ee25667aa5100
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-03-08 23:43:12 -0300

    El codigo se ejecuta sin problema
    Falta revision de profresor.

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,48 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno = document.getElementById("PrecioUno").value;
+	var precioDos = document.getElementById("PrecioDos").value;
+	var precioTres = document.getElementById("PrecioTres").value;
+	var resultado;
+	var promedio;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres =parseInt(precioTres);
 	
+	resultado = precioUno + precioDos + precioTres;
+
+	alert(" la suma de los precios es: " + " " + resultado );
 }
 function Promedio () 
 {
+	var precioUno = document.getElementById("PrecioUno").value;
+	var precioDos = document.getElementById("PrecioDos").value;
+	var precioTres = document.getElementById("PrecioTres").value;
+	var promedio;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres =parseInt(precioTres);
+	
+	promedio = (precioUno + precioDos + precioTres) / 3;
+
+	alert(" el promedio de precios es: " + " " + promedio);
 	
 }
 function PrecioFinal () 
 {
+	var precioUno = document.getElementById("PrecioUno").value;
+	var precioDos = document.getElementById("PrecioDos").value;
+	var precioTres = document.getElementById("PrecioTres").value;
+	var precioMasiva;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres =parseInt(precioTres);
 	
+	precioMasiva = (precioUno + precioDos + precioTres) * 1.21 ;
+
+	alert(" el promedio de precios es: " + " " + precioMasiva);
 }
