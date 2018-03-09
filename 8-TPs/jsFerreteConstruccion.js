1574815bad5319c1be4661aac7ac81f17f6338ac
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-03-09 01:27:06 -0300

    codio terminado falta revision del profesor
    Presentar el codigo para revision por profesor, una vez verificado y/o corregido pasarlo a lenguaje C. No olvidar comentar y subir las correciones del profesor.

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,56 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo = document.getElementById("Largo").value;
+	var ancho = document.getElementById("Ancho").value;
+	var perimetro;
+	var cantidadAlambre;
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = (2 * largo) + (2 * ancho);
+	cantidadAlambre = perimetro * 3;
+
+	alert("Cantidad de alambre: " + " " + cantidadAlambre);
 
 }
 function Circulo () 
 {
-	
+	var radio = document.getElementById("Radio").value;
+	var circunferencia;
+	var cantidadAlambre;
+
+	radio = parseInt(radio);
+
+	circunferencia = Math.PI * Math.pow(radio , 2);
+	cantidadAlambre = circunferencia * 3;
+
+	alert("la cantidad de Alambre: " + " " + cantidadAlambre);
 }
 function Materiales () 
 {
+	var largo = document.getElementById("Largo").value;
+	var ancho = document.getElementById("Ancho").value;
+	var radio = document.getElementById("Radio").value;
+	var circunferencia;
+	var perimetro;
 	
+	radio = parseInt(radio);
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+	
+	/* Para saber la cantidad de bolsas de cada material primero debemos calcular la superficie total 
+	  de cada terreno */
+
+	circunferencia = Math.PI * Math.pow(radio , 2); // Math.PI es la constante pi = 3,14...n; 
+	perimetro = (2 * largo) + (2 * ancho);			// Math.pow( base , exponente) es la forma elevar una cantidad a un exponente.
+
+	/* Luego lo dividimos las superficies en entre los materiales bolsa o cal lo cual arroja
+	   las cantidades necesarias de cada uno  */
+
+	alert("cantidad rectangulo bolsas de cemento:"+" "+ perimetro / 2 + "\n" + "cantidad rectangulo bolsas de cal:" + perimetro / 3);
+	alert("cantidad Circulo bolsas de cemento:"+" "+ circunferencia / 2 + "\n" + "cantidad Circulo bolsas de cal:" + circunferencia / 3);
+
+	/* No es recomendable hacer operaciones matematicas en las instrucciones de layout,
+	   pero en este caso las ejecute juntas por ser un codigo poco complejo */
 }
