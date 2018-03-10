fca4b73830800e4b5da77f61c6d00e102850f04b
joceduardoarias <36265049+joceduardoarias@users.noreply.github.com>
2018-03-09 23:38:49 -0300

    forma if - else
    es el algoritmo que daria respuesta utilizando if - else.

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,71 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos = 0;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
+	 numeroSecreto = parseInt(((Math.random() * (101 - 1))+1)); 
+	 console.log(numeroSecreto);
 	
 
 }
 
 function verificar()
-{
+{	// primer pedimos el dato de entra que es el numero que ingresa el usuario
+	var numero = document.getElementById("numero").value; 
 	
-	
-
+	numero = parseInt(numero);
+	// el contador se activa inmediatamente pedimos un nuevo numero al usuario
+	contadorIntentos++;
+	//ahora estructuramos las condiciones que se van a comparar
+	if (numero == numeroSecreto && contadorIntentos == 1) 
+	{
+		alert("usted es un Psíquico");
+	}
+	else
+	{
+		if (numero == numeroSecreto && contadorIntentos == 2) 
+		{
+			alert("excelente percepción");
+		}
+		else
+		{
+			if (numero == numeroSecreto && contadorIntentos == 3) 
+			{
+				alert("Esto es suerte");
+			}
+			else
+			{
+				if (numero == numeroSecreto && contadorIntentos == 4) 
+				{
+					alert("Excelente técnica");
+				}
+				else
+				{
+					if (numero == numeroSecreto && contadorIntentos == 5) 
+					{
+						alert("usted está en la media");
+					}
+					else
+					{
+						if (numero == numeroSecreto && contadorIntentos > 5 && contadorIntentos < 11) 
+						{
+							alert("falta técnica");
+						}
+						else
+						{
+							if (numero == numeroSecreto && contadorIntentos > 10) 
+							{
+								alert("afortunado en el amor!!");
+							}
+						}	
+					}	
+				}	
+			}	
+		}	
+	}	
+	document.getElementById("intentos").value = contadorIntentos;
 }
